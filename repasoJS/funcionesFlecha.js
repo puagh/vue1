e28ff9b697a7a82065ea107c3b7c4820f23bd53b
Hector Leon <puagh@outlook.com>
2022-03-11 17:57:57 -0600

    arrow functions js

--- repasoJS/funcionesFlecha.js
+++ repasoJS/funcionesFlecha.js
@@ -1,1 +1,25 @@
-console.log("Hola mundo!");
+//Function declaration
+
+function add(a, b){
+    return a + b
+}
+
+const num1 = 56, num2 = 89
+let resultado = add(num1, num2)
+console.log(`La suma de ${num1} + ${num2} es igual a ${resultado}`)
+
+
+//Function expressions
+const square = function(x, n){
+    return Math.pow(x,n)
+}
+
+const num3 = 9
+resultado = square(num2, num3)
+console.log(`${num2} a la potencia ${num3} es igual a ${resultado}`)
+
+
+//Arrow Functions
+const subtract = (a, b) => (a - b)
+resultado =  subtract(num2, num3)
+console.log(`la resta de ${num2} menos ${num3} es igual a ${resultado}`)
